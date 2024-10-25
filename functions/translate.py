c24cf127bb52369db021ba70628630eedfee8b89
pavan.pothamsetti <pavan.pothamsetti@trellix.com>
2024-10-25 15:53:51 +0530

    minor modifications

--- functions/translate.py
+++ functions/translate.py
@@ -1,12 +1,9 @@
-from subprocess import Popen,PIPE,STDOUT
 import os
 
 def main_translate(rule_id):
     dirname = os.path.dirname(__file__)
     one_file = os.path.join(dirname, 'one.py')
     args=["python3",one_file,rule_id]
-    p = Popen(args,stdout=PIPE,stderr=STDOUT,universal_newlines=True)
-    otpt = p.communicate()[0]
-    print(otpt)
-    p.kill()
+    p = os.system(" ".join(args))
+    os.remove("temp_data.json")
     print("\n\033[38:5:208mPlease add 'recommended actions' and 'tags'\033[00m")
