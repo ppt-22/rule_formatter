bc86de121dc800f77a7cc0c8b7f6042111cbc09f
pavan.pothamsetti <pavan.pothamsetti@trellix.com>
2024-10-26 16:55:48 +0530

    minor modifications

--- functions/translate.py
+++ functions/translate.py
@@ -5,5 +5,8 @@ def main_translate(rule_id):
     one_file = os.path.join(dirname, 'one.py')
     args=["python3",one_file,rule_id]
     p = os.system(" ".join(args))
-    os.remove("temp_data.json")
+    try:
+        os.remove("temp_data.json")
+    except:
+        pass
     print("\n\033[38:5:208mPlease add 'recommended actions' and 'tags'\033[00m")
